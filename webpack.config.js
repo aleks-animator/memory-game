5bf6600d7040161f6c69beac5d4f57e0621788df
aleks-animator <aleksandar.prohaska@gmail.com>
2025-01-24 01:12:19 +0100

    just dev mode for now

--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,51 @@
-const path = require('path');  // Import the path module
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 
-module.exports = (env, argv) => {
-  const isDevelopment = argv.mode === 'development';
-
-  return {
-    mode: isDevelopment ? 'development' : 'production', // Explicitly set mode
-
-    entry: './main.js',
-    output: {
-      filename: isDevelopment ? 'bundle.js' : 'bundle-[contenthash].js',
-      path: path.resolve(__dirname, 'docs'),
-      clean: true,
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: 'babel-loader',
+module.exports = {
+  entry: './main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[path][name][ext]',
         },
-        {
-          test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
-        },
-        {
-          test: /\.(png|jpe?g|gif|svg)$/i,
-          type: 'asset/resource',
-          generator: {
-            filename: 'images/[path][name][ext]',
-          },
-        },
-      ],
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: './index.html',
-        minify: isDevelopment ? false : { 
-          removeComments: true,
-          collapseWhitespace: true,
-        },
-      }),
-      new CopyPlugin({
-        patterns: [
-          { from: 'images', to: 'images' },
-        ],
-      }),
+      },
     ],
-    devServer: isDevelopment
-      ? {
-          static: path.resolve(__dirname, 'docs'),
-          compress: true,
-          port: 9000,
-          historyApiFallback: true,
-        }
-      : {},
-  };
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: 'images', to: 'images' },
+        { from: 'index.html', to: 'index.html' },
+      ],
+    }),
+  ],
+  resolve: {
+    extensions: ['.js', '.css'],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+    hot: true,
+  },
+  mode: 'development',
 };

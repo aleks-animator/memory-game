90004e7784d28bfc62edfeee45c9bea147d422bb
aleksandar <aleksandar@keen-on.de>
2025-02-11 23:01:58 +0100

    config fix, remove deps

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin'); // Add cssnano
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 const isProduction = process.env.NODE_ENV === 'production';
 
@@ -10,7 +10,7 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        publicPath: isProduction ? '/memory-game/' : '/',
+        publicPath: isProduction ? '/memory-game/' : '/', // Adjust for GitHub Pages
     },
     module: {
         rules: [
@@ -29,25 +29,30 @@ module.exports = {
                     {
                         loader: MiniCssExtractPlugin.loader,
                         options: {
-                            publicPath: isProduction ? '../' : '/',
+                            publicPath: '../', // Fix for relative paths in CSS
                         },
                     },
-                    'css-loader',
-                    'sass-loader',
+                    {
+                        loader: 'css-loader', // Translates CSS into CommonJS
+                        options: {
+                            url: false, // Disable URL transformation
+                        },
+                    },
+                    'sass-loader', // Compiles SCSS to CSS
                 ],
             },
             {
                 test: /\.(png|jpe?g|gif|svg)$/i,
                 type: 'asset/resource',
                 generator: {
-                    filename: 'images/[name][ext]',
+                    filename: 'images/[name][ext]', // Ensure images are copied to the correct folder
                 },
             },
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/i,
                 type: 'asset/resource',
                 generator: {
-                    filename: 'fonts/[name][ext]',
+                    filename: 'fonts/[name][ext]', // Ensure fonts are copied to the correct folder
                 },
             },
         ],
@@ -62,33 +67,23 @@ module.exports = {
                 },
                 {
                     from: 'fonts',
-                    to: 'fonts'
+                    to: 'fonts',
                 },
                 {
                     from: 'index.html',
-                    to: 'index.html'
+                    to: 'index.html',
                 },
             ],
         }),
         new MiniCssExtractPlugin({
-            filename: 'styles.css',
+            filename: 'styles.css', // Output CSS file name
         }),
     ],
     optimization: {
         minimize: isProduction, // Enable minimization only in production
         minimizer: [
-            `...`, // This preserves the default Webpack JS minimizer (Terser)
-            new CssMinimizerPlugin({
-                minimizerOptions: {
-                    preset: [
-                        'default',
-                        {
-                            discardComments: { removeAll: true }, // Remove all comments
-                            normalizeWhitespace: true, // Normalize whitespace
-                        },
-                    ],
-                },
-            }),
+            `...`, // Preserve the default JS minimizer (Terser)
+            new CssMinimizerPlugin(), // Use cssnano for CSS minification
         ],
     },
     resolve: {
